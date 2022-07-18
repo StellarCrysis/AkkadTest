78ba675a80fec7d2ea8a9936e81aa8f6ca000241
Grabli66 <grabli66@gmail.com>
2022-07-18 21:04:07 +0300

    Доработка окна загрузки

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
 import * as akkad from "akkad"
-import { Game } from "akkad";
 
 class GameState extends akkad.BaseState {
     async enter(): Promise<void> {
-        const camera = new akkad.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new akkad.Vector3(0, 0, 2.5), this.mainscene)
-        camera.attachControl(this.canvas)
-        camera.position = new akkad.Vector3(0, 3, 20)
-        let light = new akkad.HemisphericLight("point", new akkad.Vector3(0.1, 0.4, -1), this.mainscene);
+        game.showLoadingScreen()
 
-        let cube = akkad.MeshBuilder.CreateBox("box", {
+        const camera = new akkad.BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new akkad.BABYLON.Vector3(0, 0, 2.5), this.mainscene)
+        camera.attachControl(this.canvas)
+        camera.position = new akkad.BABYLON.Vector3(0, 3, 20)
+        let light = new akkad.BABYLON.HemisphericLight("point", new akkad.BABYLON.Vector3(0.1, 0.4, -1), this.mainscene)
+
+        let cube = akkad.BABYLON.MeshBuilder.CreateBox("box", {
             size: 2
         }, this.mainscene)
     }
 }
 
-let game = new akkad.Game("view")
+let game = new akkad.Game(document.getElementById("canvas-container"))
 let state = new GameState()
 game.setState(state)

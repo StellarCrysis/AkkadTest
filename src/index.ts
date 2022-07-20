a7e3a3daddb141c674195b0ad1ee695cca514fc2
Grabli66 <grabli66@gmail.com>
2022-07-20 20:26:11 +0300

    Доработка AssetManager

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,18 @@ import { Game } from "akkad"
 
 class GameState extends akkad.BaseState {
     async enter(): Promise<void> {
-        Game.showLoadingScreen()
+        let assetManager = new akkad.AssetManager()
+        assetManager.addMeshTask("task1", "./models/", "ship.glb")
+        let res = await assetManager.start()
 
         const camera = new akkad.BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new akkad.BABYLON.Vector3(0, 0, 2.5), this.mainscene)
         camera.attachControl(this.canvas)
         camera.position = new akkad.BABYLON.Vector3(0, 3, 20)
         let light = new akkad.BABYLON.HemisphericLight("point", new akkad.BABYLON.Vector3(0.1, 0.4, -1), this.mainscene)
 
-        let cube = akkad.BABYLON.MeshBuilder.CreateBox("box", {
-            size: 2
-        }, this.mainscene)
-
-        Game.hideLoadingScreen()
+        // let cube = akkad.BABYLON.MeshBuilder.CreateBox("box", {
+        //     size: 2
+        // }, this.mainscene)        
     }
 }
 
